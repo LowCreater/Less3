e1131ca4c7bd2644c4975e76addf61c1372b2f6c
Ivan <CHechikovIP@gmail.com>
2023-09-18 19:42:58 +0500

    Небольшое знакомство с C#

---
+++ example_001/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hello, World!");
+Console.Write("Hello, World!");
+// dotnet new console - создать новую консоль в приложении.
+// dotnet run - запустить код
+// Для вывода на экран - Console.<тип данных>(переменная или строка);
