1d0992166ba4089d3c88426754222c66ddab0b45
Ivan <CHechikovIP@gmail.com>
2023-09-18 20:19:39 +0500

    Добавили условие

---
+++ example_004/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+if (username.ToLower() == "иван")
+{
+    Console.WriteLine("Добро пожаловать! Мой Господин!");
+}
+else
+{
+    Console.WriteLine("Пошел нахер отсюдава!");
+}
