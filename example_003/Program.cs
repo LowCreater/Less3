8a37fbe4fa8db99632fc9e9c4271e9e3192dfdc4
Ivan <CHechikovIP@gmail.com>
2023-09-18 20:04:54 +0500

    Поработали с числами

---
+++ example_003/Program.cs
@@ -0,0 +1,7 @@
+﻿int numberA = 6;
+int numberB = 8;
+Console.WriteLine(numberA + numberB);
+double numberC = 13;
+double numberD = 4;
+Console.WriteLine(numberC / numberD);
+// Для работы с дробными числами используем тип данных "Double"
