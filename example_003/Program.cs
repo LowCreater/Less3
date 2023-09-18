293a4c77ead55b07330eaacb9ada3bc851a8b320
Ivan <CHechikovIP@gmail.com>
2023-09-18 20:10:32 +0500

    Добавили рандомные числа

--- example_003/Program.cs
+++ example_003/Program.cs
@@ -1,5 +1,9 @@
-﻿int numberA = 6;
-int numberB = 8;
+﻿// int numberA = 6;
+// int numberB = 8;
+int numberA = new Random().Next(1, 10); // рандомное число - 1, 2, 3, 4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10); // рандомное число - 1, 2, 3, 4 ... 9
+Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);
 double numberC = 13;
 double numberD = 4;
