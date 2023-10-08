81e42d45e777c304bff531cbd88a4abfb928f31b
Ivan <CHechikovIP@gmail.com>
2023-10-08 19:23:18 +0500

    Домашняя работа, задача3

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите день недели ");
+int number = int.Parse(Console.ReadLine());
+if (0<number || number<8)
+{
+    if (number == 6 || number == 7)
+    {
+        Console.WriteLine("Сегодня выходной");
+    }
+    else
+    {
+        Console.WriteLine("Сегодня будни!");
+    }
+}
+else 
+{   
+    Console.WriteLine("Такого дня недели нет!");
+}
